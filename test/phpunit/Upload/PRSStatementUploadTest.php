0fad21542bd32bbc2de59a39359a09d590d37eab
Greg Bowler <greg.bowler@g105b.com>
2022-11-07 14:29:41 +0000

    test: aggregate by a field

--- test/phpunit/Upload/PRSStatementUploadTest.php
+++ test/phpunit/Upload/PRSStatementUploadTest.php
@@ -24,4 +24,28 @@ class PRSStatementUploadTest extends UploadTestCase {
 		self::assertSame(0.0282, $moneyTotalUsage->value);
 		self::assertSame("£0.02", (string)$moneyTotalUsage);
 	}
+
+	public function testGetAggregatedUsageTotals():void {
+		$fileContent = <<<DATA
+		Record Number,Work Title,Amount (performance revenue)
+		1,Song 1,0.0016
+		2,Song 1,0.0016
+		3,Song 1,0.0072
+		4,Song 2,0.0036
+		5,Song 2,0.0048
+		6,Song 3,0.0081
+		7,Song 3,0.0005
+		8,Song 3,0.0006
+		9,Song 3,0.0002
+		DATA;
+		$tmpFileName = self::getTempFile($fileContent);
+		$sut = new PRSStatementUpload($tmpFileName);
+		$aggregatedTotalUsage = $sut->getAggregatedUsageTotal("Work Title");
+
+		self::assertSame(0.0104, $aggregatedTotalUsage->getTotalForAggregate("Song 1")->value);
+		self::assertSame(0.0084, $aggregatedTotalUsage->getTotalForAggregate("Song 1")->value);
+		self::assertSame(0.0094, $aggregatedTotalUsage->getTotalForAggregate("Song 1")->value);
+
+		self::assertSame(0.0282, $aggregatedTotalUsage->getTotal()->value);
+	}
 }
