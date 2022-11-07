ece972d18e018d9eda0c46e26c949371cb6509b8
Greg Bowler <greg.bowler@g105b.com>
2022-11-07 12:18:51 +0000

    wip: first test for UploadManager

--- test/phpunit/UploadManagerTest.php
+++ test/phpunit/UploadManagerTest.php
@@ -1,7 +1,29 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use Trackshift\Upload\UploadManager;
 
 class UploadManagerTest extends TestCase {
+	/** Load a file with content that isn't recognised by Trackshift. */
+	public function testLoad_unknownFileType():void {
+		$fileContent = <<<DATA
+		name,town
+		Greg,Oakwood
+		Richard,Ambergate
+		DATA;
 
+		$tmpFileName = self::getTempFile($fileContent);
+
+		$sut = new UploadManager();
+		$upload = $sut->load($tmpFileName);
+
+		self::assertInstanceOf(UnknownUpload::class, $upload);
+	}
+
+	/** @return string absolute file path of the temp file */
+	private static function getTempFile($content):string {
+		$filePath = sys_get_temp_dir() . "/" . uniqid("trackshift-test-");
+		file_put_contents($filePath, $content);
+		return $filePath;
+	}
 }
