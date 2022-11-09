fb79fda92e4860090043df78dfad59f828c3cf26
Greg Bowler <greg.bowler@g105b.com>
2022-11-09 18:05:43 +0000

    test: add first behat script

---
+++ test/behat/features/anonymous-user.feature
@@ -0,0 +1,29 @@
+Feature: App should be usable by anonymous users
+	In order to use TrackShift with as little friction as possible
+	As an anonymous user
+	I should be able to upload my data and view my royalty usages
+
+	Scenario: I get a random user ID generated
+		Given I am on the homepage
+		Then a new user ID should be generated
+
+	Scenario: I can see the upload form without logging in
+		Given I am on the homepage
+		Then I should see "Upload your statement file"
+
+	Scenario: Unknown upload types show an appropriate error message
+		Given I am on the homepage
+		When I attach the file "gubbins.txt" to "statement"
+		And I press "Upload"
+		Then I should see "ERROR: Unknown file type"
+
+	Scenario: I can upload a PRS statement
+		Given I am on the homepage
+		When I attach the file "prs-simple-3-songs.csv" to "statement"
+		And I press "Upload"
+		Then I should see 3 rows in the table
+		And I should see the following table data:
+			| Work title  | Amount |
+			| Song 2      | £0.17  |
+			| Song 1      | £0.10  |
+			| Song 3      | £0.09  |
---
+++ test/behat/features/bootstrap/FeatureContext.php
@@ -0,0 +1,84 @@
+<?php
+use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
+use Behat\Testwork\Hook\Scope\BeforeSuiteScope;
+use Gt\Daemon\Process;
+use PHPUnit\Framework\Assert as PHPUnit;
+
+class FeatureContext extends MinkContext {
+	private static Process $server;
+
+	/** @BeforeSuite */
+	public static function setUp(BeforeSuiteScope $scope):void {
+		$contextSettings = $scope->getSuite()->getSettings();
+		self::checkServerRunning(
+			$contextSettings["serverAddress"],
+			$contextSettings["serverPort"],
+		);
+	}
+
+	/** @AfterSuite */
+	public static function tearDown():void {
+		if(isset(self::$server)) {
+			self::$server->terminate();
+		}
+	}
+
+	private static function checkServerRunning(
+		string $serverAddress,
+		int $serverPort,
+	):void {
+		$socket = @fsockopen(
+			"localhost",
+			$serverPort,
+			$errorCode,
+			$errorMessage,
+			1
+		);
+		if(!$socket) {
+			if(!is_dir("www")) {
+				mkdir("www");
+			}
+			$path = realpath(__DIR__ . "/../../../../");
+			self::$server = new Process("php", "-S", "$serverAddress:$serverPort", "-t", "www", "./vendor/phpgt/webengine/go.php");
+			self::$server->setExecCwd($path);
+			self::$server->exec();
+		}
+	}
+
+	/** @Then a new user ID should be generated */
+	public function aNewUserIDShouldBeGenerated() {
+		$url = $this->getSession()->getCurrentUrl();
+		$query = parse_url($url, PHP_URL_QUERY);
+		parse_str($query, $queryParts);
+		PHPUnit::assertArrayHasKey("user", $queryParts);
+		PHPUnit::assertGreaterThan(16, strlen($queryParts["user"]));
+	}
+
+	/** @Then I should see :numRows rows in the table */
+	public function iShouldSeeRowsInTheTable(int $numRows) {
+		$table = $this->getSession()->getPage()->find("css", "table");
+		PHPUnit::assertNotNull($table, "Table not found on page");
+		$rowList = $table->findAll("css", "tbody>tr");
+		PHPUnit::assertCount($numRows, $rowList);
+	}
+
+	/** @Given I should see the following table data: */
+	public function iShouldSeeTheFollowingTableData(TableNode $data) {
+		$table = $this->getSession()->getPage()->find("css", "table");
+		$trList = $table->findAll("css", "tr");
+		$rowIndex = 0;
+
+		foreach($data->getTable() as $dataRow) {
+			$tr = $trList[$rowIndex];
+			$tdList = $tr->findAll("css", "th,td");
+
+			foreach($dataRow as $columnIndex => $text) {
+				$td = $tdList[$columnIndex];
+				PHPUnit::assertSame($text, $td->getText());
+			}
+
+			$rowIndex++;
+		}
+	}
+}
