29cd23366c5fc51aaceb6e0aa8ccacaabcdd32b2
Greg Bowler <greg.bowler@g105b.com>
2022-11-24 13:10:16 +0000

    test: automatic purging after 3 weeks

--- page/index.php
+++ page/index.php
@@ -1,29 +1,37 @@
 <?php
+use DateTime;
 use Gt\Dom\HTMLDocument;
 use Gt\DomTemplate\DocumentBinder;
-use Gt\DomTemplate\TemplateCollection;
 use Gt\Http\Response;
 use Gt\Input\Input;
 use Gt\Ulid\Ulid;
-use Trackshift\Upload\UnknownUpload;
 use Trackshift\Upload\UploadManager;
 
 function go(
+	UploadManager $uploadManager,
 	Input $input,
 	Response $response,
 	HTMLDocument $document,
 	DocumentBinder $binder,
 ):void {
+	$uploadManager->purge();
+
 	if($userId = $input->getString("user")) {
 		$userDataDir = "data/$userId";
-		if(!is_dir($userDataDir)) {
-			return;
-		}
 
-		$uploadManager = new UploadManager();
 		$statement = $uploadManager->load(...glob("$userDataDir/*.*"));
-		$binder->bindList($statement, $document->querySelector("details"));
-		$binder->bindKeyValue("uploadCount", count($statement));
+		$statementCount = $binder->bindList($statement, $document->querySelector("details"));
+		$binder->bindKeyValue("uploadCount", $statementCount);
+
+		$expiryDate = $statement->getExpiryDate();
+		$dateIn3daysMinus1day = new DateTime("+20 days");
+		if($expiryDate > $dateIn3daysMinus1day) {
+			$document->querySelector("details button[value=extend]")->hidden = true;
+		}
+		$binder->bindKeyValue("expiryDateString", $expiryDate?->format("dS M @ h:ia"));
+		if($statementCount === 0) {
+			$document->querySelector("details")->hidden = true;
+		}
 
 		$aggregatedUsages = $statement->getAggregatedUsages("workTitle");
 		$tableData = [];
@@ -90,6 +98,21 @@ function do_delete(Input $input, Response $response):void {
 	$filename = $input->getString("filename");
 	foreach(glob("data/$userId/$filename.*") as $file) {
 		unlink($file);
+	}
+
+	$response->redirect("./?user=$userId");
+}
+
+function do_extend(Input $input, Response $response):void {
+	$userId = $input->getString("user");
+	if(!$userId) {
+		$response->reload();
+	}
+
+	$dir = "data/$userId";
+	if(is_dir($dir)) {
+		$dateIn3weeks = new DateTime("+3 weeks");
+		touch($dir, $dateIn3weeks->getTimestamp());
 	}
 
 	$response->redirect("./?user=$userId");
