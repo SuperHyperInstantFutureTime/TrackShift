721be4f7639d7b9a7ae3c3b9e2ad626c456a8407
Greg Bowler <greg.bowler@g105b.com>
2023-08-26 13:16:10 +0100

    feature: cron script for background processing

--- cron/uploadProcessor.php
+++ cron/uploadProcessor.php
@@ -27,7 +27,6 @@ function go(UploadManager $uploadManager, Database $database):void {
 }
 
 // TODO: Remove all of this gumpf when https://github.com/PhpGt/WebEngine/issues/639 is implemented.
-cronLoopStart:
 chdir(dirname(__DIR__));
 require("vendor/autoload.php");
 $config = ConfigFactory::createForProject(getcwd(), "vendor/phpgt/webengine/config.default.ini");
@@ -52,6 +51,3 @@ $uploadManager = new UploadManager(
 );
 
 go($uploadManager, $database);
-sleep(1);
-echo ".";
-goto cronLoopStart;
