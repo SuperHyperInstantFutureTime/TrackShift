14e7cdca00fe798b6f8f0068a4a8c6c4f1f247fe
Greg Bowler <greg.bowler@g105b.com>
2023-10-18 17:27:39 +0100

    test: test profit calculations

--- test/behat/Context/SplitContext.php
+++ test/behat/Context/SplitContext.php
@@ -1,8 +1,7 @@
 <?php
 namespace SHIFT\Trackshift\BehatContext;
 
-use Behat\Behat\Tester\Exception\PendingException;
-use Behat\Gherkin\Node\TableNode;
+use Behat\Mink\Element\NodeElement;
 use PHPUnit\Framework\Assert as PHPUnit;
 
 class SplitContext extends PageContext {
@@ -11,5 +10,41 @@ class SplitContext extends PageContext {
 	public function iShouldSeeProductSplits(int $numSplits):void {
 		$allProductSplits = $this->page->findAll("css", "splits-list>ul>li");
 		PHPUnit::assertCount(0, $allProductSplits);
+	}
+
+	/** @Then I should see my split as :percentage */
+	public function iShouldSeeMySplitAs(string $percentage) {
+		$percentage = trim($percentage, "%");
+		$splitPercentageForms = $this->page->findAll("css", "form.split-percentage");
+		/** @var NodeElement $mySplitPercentageForm */
+		$mySplitPercentageForm = end($splitPercentageForms);
+		PHPUnit::assertSame("You", $mySplitPercentageForm->findField("owner")->getValue());
+		PHPUnit::assertSame($percentage, $mySplitPercentageForm->findField("percentage")->getValue());
+	}
+
+	/** @When I add a split of :percentage to :owner with the contact details of :contact */
+	public function iAddASplitOfToWithTheContactDetailsOf(string $percentage, string $owner, string $contact):void {
+		$percentage = trim($percentage, "%");
+		$splitPercentageForms = $this->page->findAll("css", "form.split-percentage");
+		/** @var NodeElement $mySplitPercentageForm */
+		$newSplitPercentageForm = $splitPercentageForms[count($splitPercentageForms) - 2];
+		$newSplitPercentageForm->findField("owner")->setValue($owner);
+		$newSplitPercentageForm->findField("percentage")->setValue($percentage);
+		$newSplitPercentageForm->findField("contact")->setValue($contact);
+		$newSplitPercentageForm->findButton("Add")->click();
+	}
+
+	/** @Given I should see a split for :owner of :percentage */
+	public function iShouldSeeASplitFor(string $owner, string $percentage):void {
+		$percentage = trim($percentage, "%");
+		$splitPercentageForms = $this->page->findAll("css", "form.split-percentage");
+
+		foreach($splitPercentageForms as $splitPercentageForm) {
+			if($splitPercentageForm->findField("owner")->getValue() !== $owner) {
+				continue;
+			}
+
+			PHPUnit::assertSame($percentage, $splitPercentageForm->findField("percentage")->getValue());
+		}
 	}
 }
--- test/behat/splits.feature
+++ test/behat/splits.feature
@@ -9,3 +9,33 @@ Feature: TrackShift should split my share of profits
 		And I press "Upload"
 		When I go to "/account/splits/"
 		Then I should see 0 product splits
+
+	Scenario: A product with 50/50 splits
+		Given I am on the homepage
+		And I attach the file "bandcamp-simple-3-songs.csv" to "upload[]"
+		And I press "Upload"
+		When I go to "/account/splits/"
+		And I follow "Add new split"
+		Then I should be on "/account/splits/_new/"
+
+		When I select "Person 1" from "artist"
+		And I press "Set artist"
+		And I select "BC 1" from "product"
+		And I press "Set product"
+		Then I should see my split as "100%"
+
+		When I add a split of "25%" to "Splitter 1" with the contact details of "test@test.com"
+		Then I should see my split as "75%"
+
+		When I add a split of "20%" to "Splitter 2" with the contact details of "something"
+		Then I should see my split as "55%"
+
+		When I add a split of "5%" to "Splitter 3" with the contact details of " "
+		Then I should see my split as "50%"
+		And I should see a split for "Splitter 1" of "25%"
+		And I should see a split for "Splitter 2" of "20%"
+		And I should see a split for "Splitter 3" of "5%"
+
+		When I go to "/account/products/"
+		Then I should see the total earnings for "BC 1" by "Artist 1" as "£10.85"
+		And I should see the total profit for "BC 1" by "Artist 1" as "£5.42"
