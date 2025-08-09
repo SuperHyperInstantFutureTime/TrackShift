20410deed7a27823f66f1a9196d492758e984462
Greg Bowler <greg@g105b.com>
2025-08-09 17:38:08 +0100

    alpha: new header implementation

--- page/_component/global-header.php
+++ page/_component/global-header.php
@@ -1,12 +1,20 @@
 <?php
 use Authwave\Authenticator;
 use Gt\Dom\Element;
+use Gt\Http\Uri;
 
-function go(Authenticator $authenticator, Element $element):void {
-	if($authenticator->isLoggedIn()) {
+function go(Authenticator $authenticator, Element $element, Uri $uri):void {
+	if($authenticator->isLoggedIn() && $uri->getPath() !== "/") {
 		$element->querySelector("li.login")?->remove();
 	}
 	else {
 		$element->querySelector("li.logout")?->remove();
+	}
+
+	if(str_starts_with("/account/", $uri->getPath())) {
+		$element->querySelector("menu [href='/]'")->closest("li")->hidden = true;
+	}
+	else {
+		$element->querySelector("menu [href='/account/']")->closest("li")->hidden = true;
 	}
 }
